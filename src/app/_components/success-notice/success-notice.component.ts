2957adf43d2933e80b25a6a6aa6420b514b1e7f8
vanaheimex <thegreatlichking@protonmail.com>
2021-08-11 09:52:39 -0400

    success fixes and other minor fixes

--- src/app/_components/success-notice/success-notice.component.ts
+++ src/app/_components/success-notice/success-notice.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Asset } from '@xchainjs/xchain-util';
+import { take } from 'rxjs/internal/operators/take';
 import { CopyService } from 'src/app/_services/copy.service';
+import { LayoutObserverService } from 'src/app/_services/layout-observer.service';
 
 export type noticeData = {
   copy: string;
@@ -18,8 +20,16 @@ export type noticeData = {
 })
 export class SuccessNoticeComponent implements OnInit {
   @Input() data: noticeData[];
+  isMobile: boolean;
 
-  constructor(private copyService: CopyService) {}
+  constructor(
+    private copyService: CopyService,
+    private layout: LayoutObserverService
+  ) {
+    const layout$ = this.layout.isMobile.pipe(take(1)).subscribe((res) => {
+      this.isMobile = res;
+    });
+  }
 
   copyClipboard(clip: string) {
     this.copyService.copyToClipboard(clip);
