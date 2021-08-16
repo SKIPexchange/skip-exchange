40fdff2a315f15ca50488ec6e75b0927f5624772
vanaheimex <thegreatlichking@protonmail.com>
2021-08-16 04:34:08 -0400

    FIX: cleanup the txs view + UI changes

--- src/app/_components/user-settings/user-settings-dialog/pending-txs/pending-txs-modal.component.ts
+++ src/app/_components/user-settings/user-settings-dialog/pending-txs/pending-txs-modal.component.ts
@@ -24,6 +24,7 @@ import {
   assetString,
 } from 'src/app/_services/analytics.service';
 import { LayoutObserverService } from 'src/app/_services/layout-observer.service';
+import { MockClientService } from 'src/app/_services/mock-client.service';
 
 @Component({
   selector: 'app-pending-txs-modal',
@@ -47,24 +48,16 @@ export class PendingTxsModalComponent implements OnDestroy {
   isMobile: boolean = false;
 
   constructor(
-    // public dialogRef: MatDialogRef<PendingTxsModalComponent>,
-    private explorerPathsService: ExplorerPathsService,
     private txStatusService: TransactionStatusService,
     private overlaysService: OverlaysService,
     private userService: UserService,
     private midgardService: MidgardService,
     private analytics: AnalyticsService,
-    private layout: LayoutObserverService
+    private layout: LayoutObserverService,
+    private mockClientService: MockClientService
   ) {
     this.back = new EventEmitter<null>();
     this.txs = [];
-
-    this.binanceExplorerUrl = `${this.explorerPathsService.binanceExplorerUrl}/tx`;
-    this.bitcoinExplorerUrl = `${this.explorerPathsService.bitcoinExplorerUrl}/tx`;
-    this.thorchainExplorerUrl = `${this.explorerPathsService.thorchainExplorerUrl}/txs`;
-    this.ethereumExplorerUrl = `${this.explorerPathsService.ethereumExplorerUrl}/tx`;
-    this.litecoinExplorerUrl = `${this.explorerPathsService.litecoinExplorerUrl}`;
-    this.bchExplorerUrl = `${this.explorerPathsService.bchExplorerUrl}/tx`;
 
     const pendingTxs$ = this.txStatusService.txs$.subscribe((txs) => {
       this.txs = txs;
@@ -89,13 +82,10 @@ export class PendingTxsModalComponent implements OnDestroy {
     switch (status) {
       case 'success':
         return TxStatus.COMPLETE;
-        break;
       case 'refunded':
         return TxStatus.REFUNDED;
-        break;
       default:
         return TxStatus.PENDING;
-        break;
     }
   }
 
@@ -103,22 +93,16 @@ export class PendingTxsModalComponent implements OnDestroy {
     switch (action) {
       case 'swap':
         return TxActions.SWAP;
-        break;
       case 'withdraw':
         return TxActions.WITHDRAW;
-        break;
       case 'addLiquidity':
         return TxActions.DEPOSIT;
-        break;
       case 'switch':
         return TxActions.UPGRADE_RUNE;
-        break;
       case 'refund':
         return TxActions.REFUND;
-        break;
       default:
         TxActions.SWAP;
-        break;
     }
   }
 
@@ -233,55 +217,10 @@ export class PendingTxsModalComponent implements OnDestroy {
     return asset.iconPath;
   }
 
-  explorerUrl(chain: string): string {
-    switch (chain) {
-      case 'BTC':
-        return this.bitcoinExplorerUrl;
-
-      case 'BNB':
-        return this.binanceExplorerUrl;
-
-      case 'THOR':
-        return this.thorchainExplorerUrl;
-
-      case 'ETH':
-        return this.ethereumExplorerUrl;
-
-      case 'LTC':
-        return this.litecoinExplorerUrl;
-
-      case 'BCH':
-        return this.bchExplorerUrl;
-
-      default:
-        return '';
-    }
-  }
-
-  explorerPath(tx: Tx): string {
-    if (tx.isThorchainTx && tx.chain === 'THOR') {
-      if (tx.pollThornodeDirectly || tx.pollRpc) {
-        return this.getViewBlockPath(tx.hash);
-      } else {
-        return this.thorchainExplorerUrl + '/' + tx.hash;
-      }
-    } else if (tx.chain === 'ETH') {
-      return `${this.ethereumExplorerUrl}/0x${tx.hash}`;
-    } else {
-      return this.explorerUrl(tx.chain) + '/' + tx.hash;
-    }
-  }
-
   explorerPathAlt(hash: string, chain: Chain, externalTx?: boolean): string {
     if (externalTx) chain = Chain.THORChain;
-
-    if (chain === Chain.THORChain) {
-      return this.thorchainExplorerUrl + '/' + hash;
-    } else if (chain === 'ETH') {
-      return `${this.ethereumExplorerUrl}/0x${hash}`;
-    } else {
-      return this.explorerUrl(chain) + '/' + hash;
-    }
+    hash = chain === Chain.Ethereum ? '0x' + hash : hash;
+    return this.mockClientService.getTxByChain(hash, chain);
   }
 
   exploreEvent(tx: Tx, exploreAsset: string = `${tx.chain}.${tx.ticker}`) {
@@ -334,18 +273,6 @@ export class PendingTxsModalComponent implements OnDestroy {
         exploreAsset
       );
     }
-  }
-
-  goToExternal(url: string) {
-    window.open(url, '_blank');
-  }
-
-  getViewBlockPath(hash): string {
-    let path = `https://viewblock.io/thorchain/tx/${hash}`;
-    if (environment.network === 'testnet') {
-      path += '?network=testnet';
-    }
-    return path;
   }
 
   breadcrumbNav(val: string) {
