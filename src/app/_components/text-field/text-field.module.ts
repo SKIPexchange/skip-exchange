9fbac738ac8435e3480a8330a1133f6a80cf5472
vanaheimex <thegreatlichking@protonmail.com>
2021-09-03 02:51:02 -0400

    delete module

--- src/app/_components/text-field/text-field.module.ts
+++ src/app/_components/text-field/text-field.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 import { TextFieldComponent } from './text-field.component';
 
 @NgModule({
   declarations: [TextFieldComponent],
-  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  imports: [CommonModule, FormsModule],
   exports: [TextFieldComponent],
 })
 export class TextFieldModule {}
